3944102700c296c6aa03a067ca2c662f899ecf2f
ChuanF1998 <895952166@qq.com>
2019-12-30 23:48:53 +0800

    模板测试代码

--- Class_Template/Class_Template/main.cpp
+++ Class_Template/Class_Template/main.cpp
@@ -1,3 +1,5 @@
+#if 0
+
 #include <iostream>
 using namespace std;
 
@@ -36,5 +38,134 @@ void Test_01()
 int main()
 {
 	Test_01();
+	return 0;
+}
+#endif
+
+
+#if 0
+// 定义一个模板类型的静态数组
+template<class T, size_t N = 10>
+class Array
+{
+public:
+	T& operator[](size_t index)
+	{
+		return _array[index];
+	}
+	const T& operator[](size_t index)const
+	{
+		return _array[index];
+	}
+	size_t Size()const
+	{
+		return _size;
+	}
+	bool Empty()const
+	{
+		return 0 == _size;
+	}
+private:
+	T _array[N];
+	size_t _size;
+};
+
+
+template<class T, int m = 5>
+class A
+{
+
+
+private:
+	int k ;
+	int n = m;
+};
+
+int main()
+{
+	//Array<int> arr;
+	A<int> a;
+
+	return 0;
+}
+#endif
+
+#if 0
+#include <iostream>
+using namespace std;
+
+template<class T>
+bool IsEqual(T& left, T& right)
+{
+	return left == right;
+}
+
+//函数模板特化：template<>,函数后跟<>带需特化的类型
+template<>
+bool IsEqual<char*>(char*& left, char*& right)
+{
+	if (strcmp(left, right) > 0)
+		return true;
+	return false;
+}
+
+bool IsEqual(char* left, char* right)
+{
+	if (strcmp(left, right) > 0)
+		return true;
+	return false;
+}
+
+void Test()
+{
+	char* p1 = "hello";
+	char* p2 = "aorld";
+	if (IsEqual(p1, p2))
+		cout << p1 << endl;
+	else
+		cout << p2 << endl;
+};
+
+int main()
+{
+	Test();
+	return 0;
+}
+#endif
+
+
+
+#include <iostream>
+using namespace std;
+
+
+template<class T1, class T2>
+class Data
+{
+public:
+	Data() { cout << "Data<T1, T2>" << endl; }
+private:
+	T1 _d1;
+	T2 _d2;
+};
+template<>
+class Data<int, char>
+{
+public:
+	Data() { cout << "Data<int, char>" << endl; }
+private:
+	int _d1;
+	char _d2;
+};
+void TestVector()
+{
+
+	Data<int, int> d1;
+	Data<int, char> d2;
+}
+
+int main()
+{
+	TestVector();
 	return 0;
 }
