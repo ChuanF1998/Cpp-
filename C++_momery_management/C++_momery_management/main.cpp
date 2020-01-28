e0580beb58679108312bc530a84b858a0813a029
ChuanF1998 <895952166@qq.com>
2020-01-28 22:25:02 +0800

    C++内存知识点

--- C++_momery_management/C++_momery_management/main.cpp
+++ C++_momery_management/C++_momery_management/main.cpp
@@ -17,6 +17,8 @@ int main()
 
 #endif
 
+#if 0
+
 #include <iostream>
 using namespace std;
 
@@ -25,5 +27,45 @@ int main()
 	int* k = new int(8);
 	cout << *k << endl;
 	delete k;
+	return 0;
+}
+#endif
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+//定位new表达式
+
+class student
+{
+public:
+	student(){}
+	student(int num, string name)
+		: _num(num)
+		, _name(name)
+	{}
+	friend ostream& operator<<(ostream& out, const student& s)
+	{
+		out << s._num << "  " << s._name;
+		return out;
+	}
+	void printf()
+	{
+		cout << _num << endl;
+		cout << _name << endl;
+	}
+private:
+	int _num;
+	string _name;
+};
+
+int main()
+{
+	student* s1 = new student();
+	student* s2 = new student(121, "dis");
+	student* s3 = (student*)malloc(sizeof(student));
+	new(s3)student(232, "sadja");
 	return 0;
 }
