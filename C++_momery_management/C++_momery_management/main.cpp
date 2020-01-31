13a7a4cbecebbc4966d94924d862f9269b204c89
ChuanF1998 <895952166@qq.com>
2020-01-31 23:16:52 +0800

    C++内存管理测试代码

--- C++_momery_management/C++_momery_management/main.cpp
+++ C++_momery_management/C++_momery_management/main.cpp
@@ -31,6 +31,9 @@ int main()
 }
 #endif
 
+
+#if 0
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -69,3 +72,100 @@ int main()
 	new(s3)student(232, "sadja");
 	return 0;
 }
+#endif
+
+#if 1
+//设计一个类，该类只能在堆上创建对象
+//方法一：将构造函数私有化
+class only_heap1
+{
+public:
+	static only_heap1* create(int k)
+	{
+		return new only_heap1(k);
+	}
+	static only_heap1* create()
+	{
+		return new only_heap1;
+	}
+	~only_heap1(){}
+private:
+	only_heap1(){}
+	only_heap1(int k)
+		: _k(k)
+	{}
+	only_heap1(const only_heap1& a) = delete;
+private:
+	int _k;
+};
+
+//方法二：析构函数私有化
+class only_heap2
+{
+public:
+	only_heap2(int n)
+		: _n(n)
+	{}
+
+	void destroy(only_heap2* p)
+	{
+		delete p;
+	}
+private:
+	~only_heap2(){}
+private:
+	int _n;
+};
+
+int main()
+{
+	only_heap1* q = only_heap1::create(3);
+	
+	only_heap2* o = new only_heap2(7);
+	o->destroy(o);
+	delete q;
+	return 0;
+}
+#endif
+
+
+#if 0
+//只能在栈上创建对象
+//方法一：屏蔽operator new 和 operator delete 
+class only_stack
+{
+public:
+	only_stack(){}
+	only_stack(int n)
+		: _n(n)
+	{}
+	void* operator new(size_t size) = delete;
+	void operator delete(void* v) = delete;
+private:
+	int _n;
+};
+
+//方法二：构造函数私有化
+class only_stack1
+{
+public:
+	static only_stack1 create(int n)
+	{
+		return only_stack1(n);
+	}
+private:
+	only_stack1(){}
+	only_stack1(int n)
+		: _n(n)
+	{}
+private:
+	int _n;
+};
+
+int main()
+{
+	only_stack o1(2);
+	only_stack1 o4 = only_stack1::create(5);
+	return 0;
+}
+#endif
