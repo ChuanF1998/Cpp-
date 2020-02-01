213c0fa19f581d9cc8584ce2bb19c48a5f4d6d82
ChuanF1998 <895952166@qq.com>
2020-02-01 22:30:51 +0800

    C++内存管理测试代码

--- C++_momery_management/C++_momery_management/main.cpp
+++ C++_momery_management/C++_momery_management/main.cpp
@@ -74,7 +74,7 @@ int main()
 }
 #endif
 
-#if 1
+#if 0
 //设计一个类，该类只能在堆上创建对象
 //方法一：将构造函数私有化
 class only_heap1
@@ -169,3 +169,97 @@ int main()
 	return 0;
 }
 #endif
+
+#if 0
+#include <iostream>
+//饿汉模式
+class singleton
+{
+public:
+	static singleton* get()
+	{
+		return &m_instance;
+	}
+	void print()
+	{
+		std::cout << _n << std::endl;
+	}
+private:
+	singleton(){}
+	singleton(int n)
+		: _n(n)
+	{}
+
+	singleton(const singleton& s) = delete;
+	singleton& operator = (const singleton& s) = delete;
+private:
+	int _n;
+	static singleton m_instance;
+	static int k;
+};
+
+singleton singleton::m_instance(9);
+
+int main()
+{
+	singleton::get()->print();
+	return 0;
+}
+#endif
+
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+using namespace std;
+class Singleton
+{
+public:
+	static Singleton* GetInstance() {
+		// 注意这里一定要使用Double-Check的方式加锁，才能保证效率和线程安全
+		if (nullptr == m_pInstance) {
+			m_mtx.lock();
+			if (nullptr == m_pInstance) {
+					m_pInstance = new Singleton();
+			}
+			m_mtx.unlock();
+		}
+		return m_pInstance;
+	}
+	// 实现一个内嵌垃圾回收类
+	class CGarbo {
+	public:
+		~CGarbo(){
+			if (Singleton::m_pInstance)
+				delete Singleton::m_pInstance;
+		}
+	};
+	// 定义一个静态成员变量，程序结束时，系统会自动调用它的析构函数从而释放单例对象
+	static CGarbo Garbo;
+private:
+	// 构造函数私有
+	Singleton(){};
+	// 防拷贝
+	Singleton(Singleton const&);
+	Singleton& operator=(Singleton const&);
+	static Singleton* m_pInstance; // 单例对象指针
+	static mutex m_mtx; //互斥锁
+};
+Singleton* Singleton::m_pInstance = nullptr;
+Singleton::CGarbo Garbo;
+mutex Singleton::m_mtx;
+void func(int n)
+{
+	cout << Singleton::GetInstance() << endl;
+}
+// 多线程环境下演示上面GetInstance()加锁和不加锁的区别。
+int main()
+{
+	thread t1(func, 10);
+	thread t2(func, 10);
+	t1.join();
+	t2.join();
+	cout << Singleton::GetInstance() << endl;
+	cout << Singleton::GetInstance() << endl;
+	return 0;
+}
