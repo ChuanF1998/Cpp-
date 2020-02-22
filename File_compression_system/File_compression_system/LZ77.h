3e271273d48c418b131b88464bdc9e383cbeb173
ChuanF1998 <895952166@qq.com>
2020-02-22 23:41:34 +0800

    LZ77压缩算法初步实现

--- File_compression_system/File_compression_system/LZ77.h
+++ File_compression_system/File_compression_system/LZ77.h
@@ -14,7 +14,7 @@ public:
 	void CompressFile(const string& strFilePash); //压缩算法
 	void UncompressFile(const string& strFilePath); //解压缩
 private:
-	UCH LongestMatch(USH matchHead, USH& curMatchDist);
+	UCH LongestMatch(USH matchHead, USH& curMatchDist); //获取最长匹配
 private:
 	UCH* _pWin; //用来保存待压缩数据的缓冲区
 	LZHashTable _pH;
